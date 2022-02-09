ee7b114c0685d99f517b580904e730f9f2fcb7ec
camhammond <camhammond1@gmail.com>
2022-02-08 22:12:00 -0500

    Adding shooter commands

--- src/main/cpp/commands/CmdAcquireShooterTarget.cpp
+++ src/main/cpp/commands/CmdAcquireShooterTarget.cpp
@@ -6,25 +6,25 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "commands/CmdShiftGear.h"
+#include "commands/CmdAcquireShooterTarget.h"
 
-CmdShiftGear::CmdShiftGear(SubDriveTrain* subsystem)  : m_subDriveTrain(subsystem) {
+CmdAcquireShooterTarget::CmdAcquireShooterTarget(SubLimelightShooter* subsystem) : m_subLimelightShooter(subsystem) {
   // Use addRequirements() here to declare subsystem dependencies.
   AddRequirements(subsystem);
 }
 
 // Called when the command is initially scheduled.
-void CmdShiftGear::Initialize() {}
+void CmdAcquireShooterTarget::Initialize() {}
 
 // Called repeatedly when this Command is scheduled to run
-void CmdShiftGear::Execute() {
-  m_subDriveTrain->SetDriveTrainGear();
+void CmdAcquireShooterTarget::Execute() {
+  
 }
 
 // Called once the command ends or is interrupted.
-void CmdShiftGear::End(bool interrupted) {}
+void CmdAcquireShooterTarget::End(bool interrupted) {}
 
 // Returns true when the command should end.
-bool CmdShiftGear::IsFinished() {
-  return true;
+bool CmdAcquireShooterTarget::IsFinished() {
+  return false;
 }
--- src/main/include/commands/CmdAcquireShooterTarget.h
+++ src/main/include/commands/CmdAcquireShooterTarget.h
@@ -10,7 +10,7 @@
 
 #include <frc2/command/CommandBase.h>
 #include <frc2/command/CommandHelper.h>
-#include "subsystems/SubDriveTrain.h"
+#include "subsystems/SubLimelightShooter.h"
 
 /**
  * An example command.
@@ -19,10 +19,10 @@
  * directly; this is crucially important, or else the decorator functions in
  * Command will *not* work!
  */
-class CmdShiftGear
-    : public frc2::CommandHelper<frc2::CommandBase, CmdShiftGear> {
+class CmdAcquireShooterTarget
+    : public frc2::CommandHelper<frc2::CommandBase, CmdAcquireShooterTarget> {
  public:
-  explicit CmdShiftGear(SubDriveTrain* subsystem);
+  explicit CmdAcquireShooterTarget(SubLimelightShooter* subsystem);
 
   void Initialize() override;
 
@@ -33,5 +33,6 @@ class CmdShiftGear
   bool IsFinished() override;
 
  private:
-  SubDriveTrain* m_subDriveTrain;
+
+  SubLimelightShooter* m_subLimelightShooter;
 };
