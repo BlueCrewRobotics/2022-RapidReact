282bb62d003d415e1a09cf8c7aa79e9f2a7c5e8a
camhammond <camhammond1@gmail.com>
2022-02-12 05:58:29 -0500

    Added command Stop Shooter wheels

---
+++ src/main/cpp/commands/CmdStopShooterWheels.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/CmdStopShooterWheels.h"
+
+CmdStopShooterWheels::CmdStopShooterWheels(SubShooter* subShooter) : m_subShooter(subShooter) {
+  // Use addRequirements() here to declare subsystem dependencies.
+  AddRequirements(subShooter);
+}
+
+// Called when the command is initially scheduled.
+void CmdStopShooterWheels::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void CmdStopShooterWheels::Execute() {
+  // Send 0 speed to the shooter wheels
+  m_subShooter->SpinUpWheels(0,0);
+
+}
+
+// Called once the command ends or is interrupted.
+void CmdStopShooterWheels::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool CmdStopShooterWheels::IsFinished() {
+  return true;
+}
